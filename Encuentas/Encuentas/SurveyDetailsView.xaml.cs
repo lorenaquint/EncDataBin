1ff26b25446f0b0bc9a5c0fa4b4797a8641a5ac9
lorenaquint <lorenaquint@gmail.com>
2018-04-18 12:16:43 -0500

    Initial commit

---
+++ Encuentas/Encuentas/SurveyDetailsView.xaml.cs
@@ -0,0 +1,68 @@
+﻿
+
+namespace Encuentas
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using Xamarin.Forms;
+    using Xamarin.Forms.Xaml;
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class SurveyDetailsView : ContentPage
+	{
+        private readonly string[] teams =
+
+        {
+            "Colombia",
+            "México",
+            "Perú",
+            "Argentina",
+            "Brasil",
+            "Alemania",
+            "España"
+
+        };
+		public SurveyDetailsView ()
+		{
+			InitializeComponent ();
+		}
+
+        public async void EquipoFavorito_Clicked(Object sender, EventArgs e)
+        {
+            var favoriteTeam = await DisplayActionSheet(Literals.FavoriteTeamTitle,
+                                                       null,
+                                                        null,
+                                                        teams);
+            if (!string.IsNullOrEmpty(favoriteTeam))
+            {
+                EquipoFavoritoLAbel.Text = favoriteTeam;
+            }
+        }
+        public async void Aceptar_Clicked(Object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(NombreEntry.Text) || string.IsNullOrEmpty(EquipoFavoritoLAbel.Text))
+            {
+                await DisplayAlert("Datos Incompletos",
+                                  "Recuerde Diligenciar la Información",
+                                   "Aceptar");
+                return;
+            }
+            var newSurvey = new Survey()
+            {
+                Nombre = NombreEntry.Text,
+                FechaNacimiento = FechaNacimientoDP.Date,
+                FavoriteTeam = EquipoFavoritoLAbel.Text
+            };
+            MessagingCenter.Send((ContentPage)this,
+                                 Messages.NewSurveyComplete,
+                                 newSurvey);
+
+            await Navigation.PopAsync();
+        }
+
+
+    }
+}
---
+++ Encuentas/Encuentas/SurveysView.xaml.cs
@@ -0,0 +1,32 @@
+﻿
+namespace Encuentas
+{
+    using Xamarin.Forms;
+    using Xamarin.Forms.Xaml;
+    using System;
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SurveysView : ContentPage
+    {
+        public SurveysView()
+        {
+            InitializeComponent();
+            MessagingCenter.Subscribe<ContentPage,
+            Survey>(this, Messages.NewSurveyComplete,(sender, args)
+            =>
+            {
+                SurveysPanel.Children.Add(new Label()
+                {
+                    Text = args.ToString()
+                });
+
+
+            });
+        }
+    
+        public async void AddSurvey_Clicked(Object sender, EventArgs args)
+        {
+            await Navigation.PushAsync(new SurveyDetailsView());
+        }
+
+    }
+}
